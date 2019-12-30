de9cddac48063d33b110f6966cb228c9be0a5004
堂铃 <5516023@qq.com>
2019-12-30 18:26:58 +0800

    fix: change footer styl

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,10 +3,19 @@ import React, { Component } from "react"
 class Footer extends Component {
   render() {
     return (
-      <div>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between"
+        }}
+      >
+        <span>
+          © {new Date().getFullYear()}
+          {" "}
+          <a href="https://www.gatsbyjs.org">驱动 Gatsby</a>｜
+          <a href="https://github.com/betterTisen/gatsby-omg-blog">主题 omg</a>
+        </span>
+        <span>皖ICP备19018433号</span>
       </div>
     )
   }
