7e983cb3f5234e04c7c04a8f9d40893afcd0b813
zyqTest <962726098@qq.com>
2020-01-05 17:24:27 +0800

    fix:目录样式

--- src/components/Main/NavRight/Catalogue.js
+++ src/components/Main/NavRight/Catalogue.js
@@ -3,13 +3,12 @@ import React from "react"
 function Catalogue({ data }) {
   const dom = data ? (
     <div className="Catalogue-class">
-      <div dangerouslySetInnerHTML={{ __html: data }} />
+      <div className='catalogTitle'>文章目录</div>
+      <div dangerouslySetInnerHTML={{ __html: data }} className='catalogContent'/>
     </div>
   ) : (
-    ""
-  )
-
+      ""
+    )
   return dom
 }
-
 export default Catalogue
