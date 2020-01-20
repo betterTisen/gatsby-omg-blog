18ad6dd40c19d83d839159d33b734a9e4bac299a
堂铃 <5516023@qq.com>
2020-01-20 17:29:37 +0800

    💄404 页面效果优化

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,18 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
+import Image from "gatsby-image"
 
 import SEO from "../components/SEO"
 
 class NotFoundPage extends React.Component {
   render() {
+    const imgData = this.props.data.imgData.childImageSharp.fixed
+
     return (
       <div className="ERR-404-Class">
         <SEO title="404: Not Found" />
-        <img alt="404" src={require("../../content/assets/404.jpeg")} />
-        <div>
+        <Image className="left-404-img" fixed={imgData} alt="404" />
+        <div className="right-404-context">
           <h1>404</h1>
           <p>The page you are looking for is not here.</p>
           <p>
@@ -22,3 +25,20 @@ class NotFoundPage extends React.Component {
 }
 
 export default NotFoundPage
+
+export const pageQuery = graphql`
+  query {
+    imgData: file(relativePath: { eq: "404.jpeg" }) {
+      childImageSharp {
+        fixed(width: 233, height: 300) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
